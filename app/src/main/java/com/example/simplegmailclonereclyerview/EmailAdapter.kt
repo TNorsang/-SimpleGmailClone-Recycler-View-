3f61eb3419ab962ed06257f5227dcdfa4b56b277
Norsang Nyandak <TNorsang>
2023-09-21 15:44:27 -0400

    Updated till EmailAdapter class

---
+++ app/src/main/java/com/example/simplegmailclonereclyerview/EmailAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.simplegmailclonereclyerview
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class EmailAdapter(private val emails: List<Email>) : RecyclerView.Adapter<EmailAdapter.ViewHolder>() {
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        // TODO: Create member variables for any view that will be set
+        // as you render a row.
+        val senderTextView: TextView
+        val titleTextView: TextView
+        val summaryTextView: TextView
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each sub-view
+        init {
+            // TODO: Store each of the layout's views into
+            // the public final member variables created above
+            senderTextView = itemView.findViewById(R.id.senderTv)
+            titleTextView = itemView.findViewById(R.id.titleTv)
+            summaryTextView = itemView.findViewById(R.id.summaryTv)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
